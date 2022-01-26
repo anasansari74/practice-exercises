e6eede1ab1b7e917dc4abed9451205955ae2ac95
Anas Ansari <anas_ansar1@outlook.com>
2022-01-26 19:52:24 +0000

    Build: Finished characterCounts

--- characterCount/a.js
+++ characterCount/a.js
@@ -1,36 +1,54 @@
 function count(string) {
-  if (string.length === 0) {
-    return {};
-  } else {
-    const splittedStr = string.split("");
+  // // ANSWER 1
 
-    let infoToFind = [];
+  // if (string.length === 0) {
+  //   return {};
+  // } else {
+  //   const splittedStr = string.split("");
 
-    for (let i = 0; i < splittedStr.length; i++) {
-      const toAdd = (character, count) => {
-        return {
-          character,
-          count,
-        };
-      };
+  //   let infoToFind = [];
 
-      if (i == 0) {
-        infoToFind.push(toAdd(splittedStr[i], 1));
-      } else {
-        const foundIndex = infoToFind.findIndex(
-          target => target.character == splittedStr[i]
-        );
+  //   for (let i = 0; i < splittedStr.length; i++) {
+  //     const toAdd = (character, count) => {
+  //       return {
+  //         character,
+  //         count,
+  //       };
+  //     };
 
-        if (foundIndex >= 0) {
-          infoToFind[foundIndex].count++;
-        } else {
-          infoToFind.push(toAdd(splittedStr[i], 1));
-        }
-      }
+  //     if (i == 0) {
+  //       infoToFind.push(toAdd(splittedStr[i], 1));
+  //     } else {
+  //       const foundIndex = infoToFind.findIndex(
+  //         target => target.character == splittedStr[i]
+  //       );
+
+  //       if (foundIndex >= 0) {
+  //         infoToFind[foundIndex].count++;
+  //       } else {
+  //         infoToFind.push(toAdd(splittedStr[i], 1));
+  //       }
+  //     }
+  //   }
+
+  //   return infoToFind;
+  // }
+
+  // // ANSWER 2
+
+  const splittedStr = string.split("");
+
+  const count = {};
+
+  splittedStr.forEach(item => {
+    if (count[item]) {
+      count[item] += 1;
+      return;
     }
+    count[item] = 1;
+  });
 
-    return infoToFind;
-  }
+  return count;
 }
 
 console.log(count("aba"));
